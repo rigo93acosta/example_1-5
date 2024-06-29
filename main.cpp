a7f1869ed1ade5dc9d0a56168539addf84804c65
riacosta <rigo93acosta@gmail.com>
2024-06-29 03:39:39 +0000

    Modularizando el codigo

--- main.cpp
+++ main.cpp
@@ -1,65 +1,83 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+// =====[Declaration and initialization of public global objects]=============
+DigitalIn enterButton(BUTTON1);
+DigitalIn gasDetector(D2);
+DigitalIn aButton(D5);
+DigitalIn bButton(D6);
+DigitalIn cButton(D7);
+DigitalOut alarmLed(LED2);
+DigitalOut incorrectCodeLed(D4);
+DigitalOut systemBlockedLed(D8);
+
+// =====[Declaration and initialization of public global variables]=============
+bool alarmState = OFF;
+int numberOfIncorrectCodes = 0;
+
+// ====[Declaration functions]
+void inputsInit();
+void outputsInit();
+
+void alarmActivationUpdate();
+void alarmDeactivationUpdate();
+
+
 int main()
 {
-    DigitalIn enterButton(BUTTON1);
-    DigitalIn gasDetector(D2);
-    //DigitalIn overTempDetector(D3);
-    DigitalIn aButton(D5);
-    DigitalIn bButton(D6);
-    DigitalIn cButton(D7);
-    //DigitalIn dButton(D7);
-
-    DigitalOut alarmLed(LED2);
-
-    DigitalOut incorrectCodeLed(D4);
-    DigitalOut systemBlockedLed(D8);
-
-    gasDetector.mode(PullDown);
-    //overTempDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    //dButton.mode(PullDown);
-
-    alarmLed = OFF;
-    incorrectCodeLed = OFF; //Azul
-    systemBlockedLed = OFF; //Rojo
-
-    bool alarmState = OFF;
-    int numberOfIncorrectCodes = 0;
-
+    inputsInit();
+    outputsInit();
     while (true) {
 
-        if ( gasDetector) {
-            alarmState = ON;
-        }
-
-        alarmLed = alarmState;
-
+        alarmActivationUpdate();
+        alarmDeactivationUpdate();
         //enterButton (BUTTON1) diferente al libro
         /*if (!enterButton){
             alarmState = OFF;
             alarmLed = OFF;
         }*/
-        if ( numberOfIncorrectCodes < 3 ) {
-            
-            if ( aButton && bButton && cButton && enterButton) { //Desbloquer
-                incorrectCodeLed = OFF;
-            }
 
-            if (!incorrectCodeLed && !enterButton && alarmState) {
-                if ( aButton && bButton && !cButton) { //Codigo desbloqueo
-                    alarmState = OFF;
-                    numberOfIncorrectCodes = 0;
-                } else {
-                    incorrectCodeLed = ON;
-                    numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
-                }
-            }
-        } else {
-            systemBlockedLed = ON;
+    }
+}
+
+void inputsInit(){
+    gasDetector.mode(PullDown);
+    aButton.mode(PullDown);
+    bButton.mode(PullDown);
+    cButton.mode(PullDown);
+}
+
+void outputsInit(){
+    alarmLed = OFF;
+    incorrectCodeLed = OFF; //Azul
+    systemBlockedLed = OFF; //Rojo
+}
+
+void alarmActivationUpdate(){
+    if ( gasDetector) {
+        alarmState = ON;
+    }
+
+    alarmLed = alarmState;
+}
+
+void alarmDeactivationUpdate(){
+    if ( numberOfIncorrectCodes < 3 ) {
+        
+        if ( aButton && bButton && cButton && enterButton) { //Desbloquer
+            incorrectCodeLed = OFF;
         }
+
+        if (!incorrectCodeLed && !enterButton && alarmState) {
+            if ( aButton && bButton && !cButton) { //Codigo desbloqueo
+                alarmState = OFF;
+                numberOfIncorrectCodes = 0;
+            } else {
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
+            }
+        }
+    } else {
+        systemBlockedLed = ON;
     }
 }
